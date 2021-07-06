d665cd813a2234f11b3bb058dcc24887c3dbe5b7
skva <skva1er@yandex.ru>
2021-07-06 15:50:15 +0400

    added list of students

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.time.*;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] agrs) {
@@ -19,64 +20,75 @@ public class Main {
         LocalDateTime fromConsole = LocalDateTime.of(yearOf, monthOf, dayOf, hourOf, 0);
 
         //Student date
+        ArrayList<StudentData> students = new ArrayList<>();
         StudentData st = new StudentData("Ivanov Ivan", "Java Developer",
                 LocalDateTime.of(2020, Month.JUNE, 1, 10, 0), 56);
+        StudentData st2 = new StudentData("Sidorov Ivan", "J2EE Developer",
+                LocalDateTime.of(2020, Month.JUNE, 1, 10, 0), 42);
+        students.add(st);
+        students.add(st2);
 
-        //Difference between dates
-        Duration diff = Duration.between(LocalDateTime.from(st.getDate()), LocalDateTime.from(fromConsole));
+        //For Array
+        for (int i = 0; i < students.toArray().length; i++) {
+            //Difference between dates
+            Duration diff = Duration.between(LocalDateTime.from(students.get(i).getDate()), LocalDateTime.from(fromConsole));
 
-        //Loop full days difference between
-        int numberOfDays = 0;
-        LocalDateTime date1 = st.getDate();
-        LocalDateTime date2 = fromConsole;
-        while (date1.isBefore(date2.minus(1, ChronoUnit.DAYS))) {
-            if (date1.getDayOfWeek() != (DayOfWeek.SATURDAY)
-                    && (date1.getDayOfWeek() != (DayOfWeek.SUNDAY))) {
-                numberOfDays++;
+            //Loop full days difference between
+            int numberOfDays = 0;
+            LocalDateTime date1 = students.get(i).getDate();
+            LocalDateTime date2 = fromConsole;
+            while (date1.isBefore(date2.minus(1, ChronoUnit.DAYS))) {
+                if (date1.getDayOfWeek() != (DayOfWeek.SATURDAY)
+                        && (date1.getDayOfWeek() != (DayOfWeek.SUNDAY))) {
+                    numberOfDays++;
+                }
+                date1 = date1.plus(1, ChronoUnit.DAYS);
             }
-            date1 = date1.plus(1, ChronoUnit.DAYS);
-        }
 
-        //Loop addhours difference between
-        int numberOfHours = 0;
-        LocalDateTime dateH1 = st.getDate();
-        LocalDateTime dateH2 = fromConsole;
-        if ((dateH2.getDayOfWeek() != DayOfWeek.SATURDAY) && (dateH2.getDayOfWeek() != DayOfWeek.SUNDAY)
-                && (dateH2.getHour() < 18)) {
-            while (dateH1.getHour() < dateH2.getHour()) {
-                numberOfHours++;
-                dateH2 = dateH2.minus(1, ChronoUnit.HOURS);
+            //Loop addhours difference between
+            int numberOfHours = 0;
+            LocalDateTime dateH1 = students.get(i).getDate();
+            LocalDateTime dateH2 = fromConsole;
+            if ((dateH2.getDayOfWeek() != DayOfWeek.SATURDAY) && (dateH2.getDayOfWeek() != DayOfWeek.SUNDAY)
+                    && (dateH2.getHour() < 18)) {
+                while (dateH1.getHour() < dateH2.getHour()) {
+                    numberOfHours++;
+                    dateH2 = dateH2.minus(1, ChronoUnit.HOURS);
+                }
             }
-        }
-        if (dateH2.getHour() > 18) {
-            numberOfHours = 8;
-        }
-        if ((dateH2.getDayOfWeek() == DayOfWeek.SATURDAY) || (dateH2.getDayOfWeek() == DayOfWeek.SUNDAY)) {
-            numberOfHours = 0;
-        }
+            if (dateH2.getHour() > 18) {
+                numberOfHours = 8;
+            }
+            if ((dateH2.getDayOfWeek() == DayOfWeek.SATURDAY) || (dateH2.getDayOfWeek() == DayOfWeek.SUNDAY)) {
+                numberOfHours = 0;
+            }
 
-        //Get answer difference
-        int allHours = numberOfDays * 8 + numberOfHours;
-        int answerDays = 0;
-        int answerHours = 0;
-        //Training is not finished
-        if (st.getDuration() > allHours) {
-            answerDays = (st.getDuration() - allHours) / 8;
-            answerHours = (st.getDuration() - allHours) % 8;
-        }
-        //Training is finished
-        if (st.getDuration() < allHours) {
-            answerDays = (allHours - st.getDuration()) / 8;
-            answerHours = (allHours - st.getDuration()) % 8;
-        }
-        if (st.getDuration() > allHours) {
-            System.out.println(st.getStudent() + "(" + st.getCurriculum() + ") " +
-                    " - Training is not finished. " + answerDays + " d "
-                            + answerHours + " hours are left until the end.");
-        } if (st.getDuration() < allHours) {
-            System.out.println(st.getStudent() + "(" + st.getCurriculum() + ")" +
-                    " - Training completed. " + answerDays + " d "
-                    + answerHours + " hours have passed since the end.");
+            //Get answer difference
+            int allHours = numberOfDays * 8 + numberOfHours;
+            int answerDays = 0;
+            int answerHours = 0;
+            //Training is not finished
+            if (students.get(i).getDuration() > allHours) {
+                answerDays = (students.get(i).getDuration() - allHours) / 8;
+                answerHours = (students.get(i).getDuration() - allHours) % 8;
+            }
+            //Training is finished
+            if (students.get(i).getDuration() < allHours) {
+                answerDays = (allHours - students.get(i).getDuration()) / 8;
+                answerHours = (allHours - students.get(i).getDuration()) % 8;
+            }
+
+            //Print short answer
+            if (students.get(i).getDuration() > allHours) {
+                System.out.println(students.get(i).getStudent() + " (" + students.get(i).getCurriculum() + ")" +
+                        " - Training is not finished. " + answerDays + " d "
+                        + answerHours + " hours are left until the end.");
+            }
+            if (students.get(i).getDuration() < allHours) {
+                System.out.println(students.get(i).getStudent() + " (" + students.get(i).getCurriculum() + ")" +
+                        " - Training completed. " + answerDays + " d "
+                        + answerHours + " hours have passed since the end.");
+            }
         }
     }
 }
